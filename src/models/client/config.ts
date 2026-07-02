c7648847ef51c4d0e6c81ce706896f913d77b2ca
Raj Mishra <raj212558@gmail.com>
2026-07-02 18:41:15 +0530

    Add logging for appwrite configuration
    Log appwrite endpoint and project ID for debugging.

--- src/models/client/config.ts
+++ src/models/client/config.ts
@@ -1,6 +1,9 @@
 import env from "@/app/env";
 import { Client, Account, Avatars, Databases, Storage } from "appwrite";
 
+console.log("Endpoint:", env.appwrite.endpoint);
+console.log("Project:", env.appwrite.projectId);
+
 const client = new Client()
   .setEndpoint(env.appwrite.endpoint) // Your API Endpoint
   .setProject(env.appwrite.projectId); // Your project ID
