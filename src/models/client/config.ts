1688988d92490b9038d835dc3b4e02b8b62b5dee
Raj Mishra <raj212558@gmail.com>
2026-07-03 09:52:33 +0530

    fix: remove logs of env variables

--- src/models/client/config.ts
+++ src/models/client/config.ts
@@ -1,9 +1,6 @@
 import env from "@/app/env";
 import { Client, Account, Avatars, Databases, Storage } from "appwrite";
 
-console.log("Endpoint:", env.appwrite.endpoint);
-console.log("Project:", env.appwrite.projectId);
-
 const client = new Client()
   .setEndpoint(env.appwrite.endpoint)
   .setProject(env.appwrite.projectId); // Your project ID
