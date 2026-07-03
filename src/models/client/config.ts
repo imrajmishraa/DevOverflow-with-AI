f31771749fc02d3de922e0fba0c6fe6d285a54b0
Raj Mishra <raj212558@gmail.com>
2026-07-03 09:27:15 +0530

    fix: env issue causes undefined projectId

--- src/models/client/config.ts
+++ src/models/client/config.ts
@@ -5,14 +5,12 @@ console.log("Endpoint:", env.appwrite.endpoint);
 console.log("Project:", env.appwrite.projectId);
 
 const client = new Client()
-  .setEndpoint(env.appwrite.endpoint) // Your API Endpoint
+  .setEndpoint(env.appwrite.endpoint)
   .setProject(env.appwrite.projectId); // Your project ID
 
-const databases = new Databases(client);  
+const databases = new Databases(client);
 const account = new Account(client);
 const avatars = new Avatars(client);
 const storage = new Storage(client);
 
-export { client, databases, account, avatars, storage}
-
-
+export { client, databases, account, avatars, storage };
